e0174361b666e273d77fce1bb97ec764a2473eeb
Brando <brando90@mit.edu>
2016-06-19 19:08:45 -0400

    numpy add works

--- broad_cast_numpy_example_constant.py
+++ broad_cast_numpy_example_constant.py
@@ -5,8 +5,12 @@ D = 2
 D1 = 3
 x = np.random.rand(M,D)
 W = np.random.rand(D,D1)
-WW = np.sum( W, axis=0)
-XX = np.sum( x, axis=1)
+WW = np.sum( W, axis=0, keepdims=True) # D1 x 1
+XX = np.sum( x, axis=1, keepdims=True) # D x 1
 
+print WW
+print XX
+
+#P = WW.reshape(1,D1)+XX.reshape(5,1)
 P = WW+XX
 print P
