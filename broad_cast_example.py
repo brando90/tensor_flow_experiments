c67f79ceffa42255e54e85dd6de423a539c0384d
Brando <brando90@mit.edu>
2016-06-19 19:30:52 -0400

    broadcasting worked for delta tilde

--- broad_cast_example.py
+++ broad_cast_example.py
@@ -10,8 +10,8 @@ D1 = 3
 x = tf.placeholder(tf.float32, shape=[None, D]) # M x D
 # Variable is a value that lives in TensorFlow's computation graph
 W = tf.Variable( tf.truncated_normal([D,D1], mean=0.0, stddev=0.1) ) # (D x D1)
-WW =  tf.reduce_sum(W*W, reduction_indices=0) #( 1 x D^(l)= sum( (D^(l-1) x D^(l)), 0 )
-XX =  tf.reduce_sum(x*x, reduction_indices=1) # (M x 1) = sum( (M x D^(l-1)), 1 )
+WW =  tf.reduce_sum(W*W, reduction_indices=0, keep_dims=True) #( 1 x D^(l)= sum( (D^(l-1) x D^(l)), 0 )
+XX =  tf.reduce_sum(x*x, reduction_indices=1, keep_dims=True) # (M x 1) = sum( (M x D^(l-1)), 1 )
 #Delta_tilde = 2.0*tf.matmul(x,W) - (WW + XX) # (M x D^(l)) - (M x D^(l)) = (M x D^(l-1)) * (D^(l-1) x D^(l)) - (M x D^(l))
 P1 = tf.add(WW, XX)
 P2 = 2.0*tf.matmul(x,W)
