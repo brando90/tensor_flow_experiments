47d492f7232d9612e7250b7b04684c1384a5df4f
Brando <brando90@mit.edu>
2016-06-19 19:31:34 -0400

    rbf did not crash

--- first_attempt_RBF.py
+++ first_attempt_RBF.py
@@ -38,8 +38,8 @@ S = tf.Variable(tf.constant(0.001, shape=[1])) # (1 x 1)
 C = tf.Variable( tf.truncated_normal([D1,1], mean=0.0, stddev=0.1) ) # (D1 x 1)
 
 # make model
-WW =  tf.reduce_sum(W*W, reduction_indices=0) #( 1 x D^(l)= sum( (D^(l-1) x D^(l)), 0 )
-XX =  tf.reduce_sum(x*x, reduction_indices=1) # (M x 1) = sum( (M x D^(l-1)), 1 )
+WW =  tf.reduce_sum(W*W, reduction_indices=0, keep_dims=True) #( 1 x D^(l)= sum( (D^(l-1) x D^(l)), 0 )
+XX =  tf.reduce_sum(x*x, reduction_indices=1, keep_dims=True) # (M x 1) = sum( (M x D^(l-1)), 1 )
 #Delta_tilde = 2.0*tf.matmul(x,W) - (WW + XX) # (M x D^(l)) - (M x D^(l)) = (M x D^(l-1)) * (D^(l-1) x D^(l)) - (M x D^(l))
 P1 = tf.add(WW, XX)
 P2 = 2.0*tf.matmul(x,W)
