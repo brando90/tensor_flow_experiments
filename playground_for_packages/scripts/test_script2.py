0a2a6ba3d9a60c85dc23d01cd6912cdafc9f7b68
Brando <brando90@mit.edu>
2016-06-26 14:27:05 -0400

    added some comments to my example scripts that you need to specify package names for importing

--- playground_for_packages/scripts/test_script2.py
+++ playground_for_packages/scripts/test_script2.py
@@ -1,9 +1,7 @@
-# imports the __init__.py module which in this special example has loaded all the modules in its package/folder
-# so you can use all your functions in that package due to the imports in the __ini__.py from pkg_1
-import pkg_1
+import pkg_1.module1 as m1
 # line bellow does not help to run f2. Notice that pip (package manager) points to
 # my_proj directiory. This probably means it needs the package names to import things since sys.path points to
 # my_project. So say the project name to help sys.path find your modules (that should be inside packages)
 # import module1 as m1
 
-pkg_1.f1()
+m1.f2()
