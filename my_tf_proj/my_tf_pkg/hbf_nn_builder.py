73fb52c5731ce5fe2c785fa259699e198267ab6b
Brando <brando90@mit.edu>
2016-06-27 18:34:57 -0400

    saving changes

--- my_tf_proj/my_tf_pkg/hbf_nn_builder.py
+++ my_tf_proj/my_tf_pkg/hbf_nn_builder.py
@@ -4,51 +4,23 @@ import tensorflow as tf
 def hello_world():
     print "Hello World!"
 
-## Summated HBF
-
-def get_summed_Gaussian_layer(l, x, dims, init, phase_train=None, scope="SumNNLayer"):
-    (init_C,init_W,init_S) = init
-    with tf.name_scope("Z"+l) as scope:
-        W = tf.Variable(init_W, name='W'+l, trainable=True)
-        S = tf.Variable(init_S, name='S'+l, trainable=True)
-        WW =  tf.reduce_sum(W*W, reduction_indices=0, keep_dims=True) #( 1 x D^(l)= sum( (D^(l-1) x D^(l)), 0 )
-        XX =  tf.reduce_sum(x*x, reduction_indices=1, keep_dims=True) # (M x 1) = sum( (M x D^(l-1)), 1 )
-        # -|| x - w ||^2 = -(-2<x,w> + ||x||^2 + ||w||^2) = 2<x,w> - (||x||^2 + ||w||^2)
-        Delta_tilde = 2.0*tf.matmul(x,W) - tf.add(WW, XX) # (M x D^(l)) - (M x D^(l)) = (M x D^(l-1)) * (D^(l-1) x D^(l)) - (M x D^(l))
-        if phase_train is not None:
-            Delta_tilde = standard_batch_norm(Delta_tilde, 1,phase_train)
-        beta = 0.5*tf.pow(tf.div(1.0,S), 2)
-        Z = beta * ( Delta_tilde ) # (M x D^(l))
-    with tf.name_scope("A"+l) as scope:
-        A = tf.exp(Z) # (M x D^(l))
-    with tf.name_scope("SumGauss"+l) as scope:
-        layer = get_summation_layer(l, a, init_C, scope="SumLayer"+l)
-        #C = tf.Variable(init_C, name='C'+l, trainable=True)
-        #layer = tf.matmul(A,C) # (M x 1) = (M x D^(l)) * (D^(l) x 1)
-    return layer
+## builders for Networks
 
 def build_summed_HBF(x, dims, inits, phase_train=None):
     (inits_C,inits_W,inits_S) = inits
     layer = x
     nb_hidden_layers = len(dims)-1
     for l in xrange(1,nb_hidden_layers): # from 1 to L-1
-        layer = get_summed_Gaussian_layer(l=str(l), x=layer, init=(inits_C[l],inits_W[l],inits_S[l]), dims=(dims[l-1], dims[l]), phase_train=phase_train, scope='HBFLayer'+str(l))
+        layer = get_HBF_layer(l=str(l),x=layer,init=(inits_W[l],inits_S[l]),dims=(dims[l-1],dims[l]),phase_train=phase_train)
+        layer = get_summation_layer(str(l),layer,inits_C[l])
     return layer
 
-## Summated NN
-
-def get_summed_NN_layer(l, x, dims, init, phase_train=None, scope="SumNNLayer"):
-    (init_C,init_W,init_b) = init
-    with tf.name_scope(scope):
-        W = tf.Variable(init_W, name='W'+l, trainable=True)
-        b = tf.Variable(init_b, name='b'+l, trainable=True)
-        z = tf.matmul(x,W) + b
-        if phase_train is not None:
-            z = standard_batch_norm(z, 1, phase_train,scope='BN'+l)
-        a = tf.nn.relu(z) # (M x D1) = (M x D) * (D x D1)
-        layer = get_summation_layer(l, a, init_C, scope="SumLayer"+l)
-        W = tf.histogram_summary('W'+l, W)
-        b = tf.histogram_summary('b'+l, b)
+def build_HBF(x, dims, inits, phase_train=None):
+    (_,inits_W,inits_S) = inits
+    layer = x
+    nb_hidden_layers = len(dims)-1
+    for l in xrange(1,nb_hidden_layers): # from 1 to L-1
+        layer = get_HBF_layer(l=str(l),x=layer,init=(inits_W[l],inits_S[l]),dims=(dims[l-1],dims[l]),phase_train=phase_train)
     return layer
 
 def build_summed_NN(x, dims, inits, phase_train=None):
@@ -56,22 +28,8 @@ def build_summed_NN(x, dims, inits, phase_train=None):
     layer = x
     nb_hidden_layers = len(dims)-1
     for l in xrange(1,nb_hidden_layers): # from 1 to L-1
-        layer = get_summed_NN_layer(l=str(l), x=layer, init=(inits_C[l],inits_W[l],inits_b[l]), dims=(dims[l-1], dims[l]), phase_train=phase_train, scope='NNLayer'+str(l))
-    return layer
-
-## Standardn NN
-
-def get_standard_NN_layer(l, x, dims, init, phase_train=None, scope="NNLayer"):
-    (init_W,init_b) = init
-    with tf.name_scope(scope):
-        W = tf.Variable(init_W, name='W'+l, trainable=True)
-        b = tf.Variable(init_b, name='b'+l, trainable=True)
-        z = tf.matmul(x,W) + b
-        if phase_train is not None:
-            z = standard_batch_norm(z, 1, phase_train,scope='BN'+l)
-        layer = tf.nn.relu(z) # (M x D1) = (M x D) * (D x D1)
-        W = tf.histogram_summary('W'+l, W)
-        b = tf.histogram_summary('b'+l, b)
+        layer = get_NN_layer(str(l),layer,dims,(inits_W[l],inits_b[l]), phase_train=None)
+        layer = get_summation_layer(str(l),layer,inits_C[l])
     return layer
 
 def build_standard_NN(x, dims, inits, phase_train=None):
@@ -79,23 +37,54 @@ def build_standard_NN(x, dims, inits, phase_train=None):
     layer = x
     nb_hidden_layers = len(dims)-1
     for l in xrange(1,nb_hidden_layers): # from 1 to L-1
-        layer = get_NN_layer(l=str(l), x=layer, init=(inits_W[l],inits_b[l]), dims=(dims[l-1], dims[l]), phase_train=phase_train, scope='NNLayer'+str(l))
+        layer = get_NN_layer(l=str(l), x=layer, init=(inits_W[l],inits_b[l]), dims=(dims[l-1], dims[l]), phase_train=phase_train)
     return layer
 
-## Final Layer
+## build layers blocks NN
 
 def get_summation_layer(l, x, init, scope="SumLayer"):
-    with tf.name_scope(scope):
+    with tf.name_scope(scope+l):
         C = tf.Variable( init )
-        a = tf.matmul(x, C)
-        C = tf.histogram_summary("C"+l, C)
-    return a
+        layer = tf.matmul(x, C)
+    C = tf.histogram_summary("C"+l, C)
+    return layer
 
-## BN
+def get_HBF_layer(l, x, dims, init, phase_train=None, scope="NNLayer"):
+    (init_W,init_S) = init
+    with tf.name_scope(scope+l):
+        with tf.name_scope('Z'+l):
+            W = tf.Variable(init_W, name='W'+l, trainable=True, dtype=tf.float64)
+            S = tf.Variable(init_S, name='S'+l, trainable=True, dtype=tf.float64)
+            WW =  tf.reduce_sum(W*W, reduction_indices=0, keep_dims=True) #( 1 x D^(l)= sum( (D^(l-1) x D^(l)), 0 )
+            XX =  tf.reduce_sum(x*x, reduction_indices=1, keep_dims=True) # (M x 1) = sum( (M x D^(l-1)), 1 )
+            # -|| x - w ||^2 = -(-2<x,w> + ||x||^2 + ||w||^2) = 2<x,w> - (||x||^2 + ||w||^2)
+            Delta_tilde = 2.0*tf.matmul(x,W) - tf.add(WW, XX) # (M x D^(l)) - (M x D^(l)) = (M x D^(l-1)) * (D^(l-1) x D^(l)) - (M x D^(l))
+            if phase_train is not None:
+                Delta_tilde = standard_batch_norm(Delta_tilde, 1,phase_train)
+            beta = 0.5*tf.pow(tf.div( tf.constant(1.0,dtype=tf.float64),S), 2)
+            Z = beta * ( Delta_tilde ) # (M x D^(l))
+        with tf.name_scope('A'+l):
+            layer = tf.exp(Z) # (M x D^(l))
+    W = tf.histogram_summary('W'+l, W)
+    b = tf.histogram_summary('S'+l, S)
+    return layer
+
+def get_NN_layer(l, x, dims, init, phase_train=None, scope="NNLayer"):
+    (init_W,init_b) = init
+    with tf.name_scope(scope+l):
+        W = tf.Variable(init_W, name='W'+l, trainable=True)
+        b = tf.Variable(init_b, name='b'+l, trainable=True)
+        z = tf.matmul(x,W) + b
+        if phase_train is not None:
+            z = standard_batch_norm(z, 1, phase_train,scope='BN'+l)
+        layer = tf.nn.relu(z) # (M x D1) = (M x D) * (D x D1)
+    W = tf.histogram_summary('W'+l, W)
+    b = tf.histogram_summary('b'+l, b)
+    return layer
 
 def standard_batch_norm(x, n_out, phase_train, scope='bn'):
     """
-    Batch normalization on convolutional maps.
+    Batch normalization on feedforward maps.
     Args:
         x:           Vector
         n_out:       integer, depth of input maps
--- tf_experiments_scripts/main_nn.py
+++ tf_experiments_scripts/main_nn.py
@@ -67,13 +67,23 @@ def get_initilizations_HBF(init_args):
         for l in range(1,nb_hidden_layers):
             inits_W.append( tf.truncated_normal(shape=[1,init_args.dims[l]], mean=init_args.mu[l], stddev=init_args.std[l], dtype=tf.float64) )
             inits_S.append( tf.constant(init_args.S_init[l], shape=[dims[l]], dtype=tf.float64 ) )
-            inits_C.append( tf.truncated_normal(shape=[init_args.dims[l],1], mean=init_args.mu[l], stddev=init_args.std[l], dtype=tf.float64) )
+            #inits_C.append( tf.truncated_normal(shape=[init_args.dims[l],1], mean=init_args.mu[l], stddev=init_args.std[l], dtype=tf.float64) )
+        l = len(init_args.dims)-1
+        inits_C = [ tf.truncated_normal(shape=[init_args.dims[l-1],init_args.dims[l]], mean=init_args.mu, stddev=init_args.std, dtype=tf.float64) ]
     elif init_args.init_type  == 'data_init':
-        D1 = init_args.dims[l]
-        indices = np.random.choice(D1,size=4)
-        X_train = init_args.X_train
-        inits_W = X_train[indices,:]
-        pass
+        nb_hidden_layers = len(dims)-1
+        inits_W = [None]
+        inits_S = [None]
+
+        indices = np.random.choice(X_train.shape[0],size=init_args.dims[1])
+        #print np.transpose(init_args.X_train[indices,:]).shape
+        inits_W.append(np.transpose(init_args.X_train[indices,:]) )
+        for l in range(1,nb_hidden_layers):
+            #inits_W.append( tf.truncated_normal(shape=[1,init_args.dims[l]], mean=init_args.mu[l], stddev=init_args.std[l], dtype=tf.float64) )
+            inits_S.append( tf.constant(init_args.S_init[l], shape=[dims[l]], dtype=tf.float64 ) )
+            #inits_C.append( tf.truncated_normal(shape=[init_args.dims[l],1], mean=init_args.mu[l], stddev=init_args.std[l], dtype=tf.float64) )
+        l = len(init_args.dims)-1
+        inits_C = [ tf.truncated_normal(shape=[init_args.dims[l-1],init_args.dims[l]], mean=init_args.mu, stddev=init_args.std, dtype=tf.float64) ]
     return (inits_C,inits_W,inits_S)
 
 ## Data sets
@@ -82,30 +92,31 @@ def get_initilizations_HBF(init_args):
 (N_test,D_out) = Y_test.shape
 
 ## NN params
-bn = True
-if bn:
-    phase_train = tf.placeholder(tf.bool, name='phase_train') ##BN ON
-else:
-    phase_train = None
-
 dims = [D,24,D_out]
 #dims = [D,24,24,D_out]
 #dims = [D,24,24,24,D_out]
 #dims = [D,24,24,24,24,D_out]
 mu = len(dims)*[0.0]
 std = len(dims)*[0.1]
-b_init = len(dims)*[0.1]
+init_constant = 100
+b_init = len(dims)*[init_constant]
 S_init = b_init
 init_type = 'truncated_normal'
 init_type = 'data_init'
-init_args = ns.FrozenNamespace(init_type=init_type,dims=dims,mu=mu,std=std,b_init=b_init,S_init=S_init)
+init_args = ns.FrozenNamespace(init_type=init_type,dims=dims,mu=mu,std=std,b_init=b_init,S_init=S_init, X_train=X_train)
 model = 'standard_nn'
 model = 'summed_nn'
 model = 'hbf'
 #model = 'summed_hbf'
+
+bn = True
+if bn:
+    phase_train = tf.placeholder(tf.bool, name='phase_train') ##BN ON
+else:
+    phase_train = None
+
 ## Make Model
 x = tf.placeholder(tf.float64, shape=[None, D], name='x-input') # M x D
-print x
 nb_layers = len(dims)-1
 nb_hidden_layers = nb_layers-1
 print( '-----> Running model: %s. (nb_hidden_layers = %d, nb_layers = %d)' % (model,nb_hidden_layers,nb_layers) )
@@ -123,9 +134,10 @@ elif model == 'summed_nn':
         mdl = mtf.build_summed_NN(x,dims,(inits_C,inits_W,inits_b),phase_train)
 elif model == 'hbf':
     tensorboard_data_dump = '/tmp/hbf_logs'
-    (inits_C,inits_W,inits_S) = get_initilizations_summed_HBF(init_args)
+    (inits_C,inits_W,inits_S) = get_initilizations_HBF(init_args)
     with tf.name_scope("HBF") as scope:
         mdl = mtf.build_HBF(x,dims,(inits_C,inits_W,inits_S),phase_train)
+        mdl = mtf.get_summation_layer(l=str(nb_layers),x=mdl,init=inits_C[0])
 elif model == 'summed_hbf':
     tensorboard_data_dump = '/tmp/summed_hbf_logs'
     (inits_C,inits_W,inits_S) = get_initilizations_summed_HBF(init_args)
@@ -138,18 +150,21 @@ y_ = tf.placeholder(tf.float64, shape=[None, D_out]) # (M x D)
 with tf.name_scope("L2_loss") as scope:
     l2_loss = tf.reduce_mean(tf.square(y_-y))
 
-## Training Step
+## train params
+report_error_freq = 100
+steps = 120000
+M = 1000 #batch-size
 optimization_alg = 'GD'
 optimization_alg = 'Momentum'
 optimization_alg = 'Adadelta'
 optimization_alg = 'Adam'
-optimization_alg = 'Adagrad'
-optimization_alg = 'RMSProp'
+#optimization_alg = 'Adagrad'
+#optimization_alg = 'RMSProp'
 with tf.name_scope("train") as scope:
-    starter_learning_rate = 0.00001
+    starter_learning_rate = 0.1
     decay_rate = 0.80
     decay_steps = 1000
-    staircase = False
+    staircase = True
     # decayed_learning_rate = learning_rate * decay_rate ^ (global_step / decay_steps)
     global_step = tf.Variable(0, trainable=False)
     learning_rate = tf.train.exponential_decay(learning_rate=starter_learning_rate, global_step=global_step,decay_steps=decay_steps, decay_rate=decay_rate, staircase=staircase)
@@ -205,14 +220,12 @@ with tf.Session() as sess:
     writer = tf.train.SummaryWriter(tensorboard_data_dump, sess.graph)
 
     sess.run( tf.initialize_all_variables() )
-    steps = 120000
-    M = 1000 #batch-size
     for i in range(steps):
         ## Create fake data for y = W.x + b where W = 2, b = 0
         #(batch_xs, batch_ys) = get_batch_feed(X_train, Y_train, M, phase_train)
         feed_dict_batch = get_batch_feed(X_train, Y_train, M, phase_train)
         ## Train
-        if i%1 == 0:
+        if i%report_error_freq == 0:
             train_result = sess.run([merged, l2_loss], feed_dict=feed_dict_train)
             summary_str_train = train_result[0]
             train_error = train_result[1]
@@ -224,7 +237,7 @@ with tf.Session() as sess:
             writer.add_summary(summary_str_train, i)
             #print("step %d, training error %g"%(i, train_error))
             print("Model *%s%s*, step %d, training error %g, test error %g"%(model, nb_hidden_layers, i, train_error,test_error))
-            print("Opt: %s, BN %s, After %d iteration:" % (optimization_alg,bn,i) )
+            print("Opt: %s, BN %s, After %d iteration, Init: %s" % (optimization_alg,bn,i,init_type) )
         sess.run(train_step, feed_dict=feed_dict_batch)
         #sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
