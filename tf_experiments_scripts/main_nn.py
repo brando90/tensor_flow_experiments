dcee0887bb62f148b4bbf613a4583383ce0a3d69
Brando <brando90@mit.edu>
2016-07-03 01:08:19 -0400

    changes

--- tf_experiments_scripts/main_nn.py
+++ tf_experiments_scripts/main_nn.py
@@ -20,6 +20,9 @@ import namespaces as ns
 #import winsound
 
 def make_and_check_dir(path):
+    '''
+        tries to make dir/file, if it exists already does nothing else creates it.
+    '''
     try:
         os.makedirs(path)
     except OSError:
