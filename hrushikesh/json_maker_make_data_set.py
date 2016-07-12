9209fc28a94773b859a3f9d4f088a8da9cdb3d1f
Brando <brando90@mit.edu>
2016-07-12 15:12:50 -0400

    data sets made

--- hrushikesh/json_maker_make_data_set.py
+++ hrushikesh/json_maker_make_data_set.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import pdb
 
 with open('hrushikesh_patient_data.json', 'r') as f_json:
     patients_data = json.load(f_json)
@@ -12,10 +13,11 @@ with open('hrushikesh_patient_data.json', 'r') as f_json:
 
 #patient_data[PtID]['X'] -> X
 #patient_data[PtID]['Y'] -> Y
+#patient_data_set[]
 m = 6
 T = 20
-for PtID,current_SensorGLU_list  in data.iteritems():
-    #current_SensorGLU_list = patient_data[PtID] # sugar_list = [..,s,..]
+for PtID,_  in patients_data.iteritems():
+    current_SensorGLU_list = patients_data[PtID]['SensorGLU_list'] # sugar_list = [..,s,..]
     patients_data[PtID]['X'] = []
     patients_data[PtID]['Y'] = []
     for i,sugar in enumerate(current_SensorGLU_list):
@@ -26,10 +28,10 @@ for PtID,current_SensorGLU_list  in data.iteritems():
         else:
             x = current_SensorGLU_list[i:i+m] # vector in R^(m)
             y_1 = current_SensorGLU_list[i+m+T]
-            y_2 = current_SensorGLU_list[i+m+T+1]
+            y_2 = current_SensorGLU_list[i+m+T+1]/10
             y = [y_1, y_2]
             patients_data[PtID]['X'].append(x)
             patients_data[PtID]['Y'].append(y)
 
 with open('patient_data_X_Y.json', 'w+') as f_json:
-    json.dump(patient_data,f_json,sort_keys=True, indent=2, separators=(',', ': '))
+    json.dump(patients_data,f_json,sort_keys=True, indent=2, separators=(',', ': '))
