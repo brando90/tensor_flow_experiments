c46e6336e9a40d1ea6e3643930a5d3f5a0a776e4
Brando <brando90@mit.edu>
2016-07-16 01:14:34 -0400

    script that process om experimetns

--- tf_experiments_scripts/collect_results.py
+++ tf_experiments_scripts/collect_results.py
@@ -9,23 +9,26 @@ job_name = 'HBF1_12_multiple_S' # <- job_name
 job_name
 current_experiment_folder = '/%s_%s_j%s'%(prefix,date,job_name)
 path = path_root+current_experiment_folder
+#
+best_test_errors = float("inf")
+best_test_filname = None
 #for slurm_array_task_id in range(1,101):
 for filename in os.listdir(path):
     #mdl_dir ='/mdls_%s_%s_slurm_sj%s'%(prefix,date,slurm_array_task_id)
     #json_file = '/%s_json_%s_slurm_sj%s'%(prefix,date,slurm_array_task_id)
     #path_to_json_file = path + json_file
     if 'json' in filename:
-        best_test_errors = float("inf")
-        best_test_filname = None
-        with open(filename, 'r') as fjson:
+        path_to_json_file = path+'/'+filename
+        with open(path_to_json_file, 'r') as data_file:
             results = json.load(data_file)
             #'train_errors':[], 'cv_errors':[],'test_errors':[]
             train_errors = results['train_errors']
             cv_errors = results['cv_errors']
             test_errors = results['test_errors']
             test_error = test_errors[-1]
+            print test_error
             if test_error < best_test_errors:
-                best_test_errors = best_test_errors
+                best_test_errors = test_error
                 best_test_filname = filename
 
 print 'best_test_errors ', best_test_errors
