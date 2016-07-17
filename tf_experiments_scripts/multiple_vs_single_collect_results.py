a1f5d969d72373cdf1130673f1b082c016f44c10
Brando <brando90@mit.edu>
2016-07-17 15:03:36 -0400

    wrong results

--- tf_experiments_scripts/multiple_vs_single_collect_results.py
+++ tf_experiments_scripts/multiple_vs_single_collect_results.py
@@ -1,6 +1,24 @@
 import json
 import os
 import pdb
+import krls
+import matplotlib.pyplot as plt
+
+##
+def get_list_errors(experiment_results):
+    # experiment_results : units->results
+    list_units = []
+    list_test_errors = []
+    for nb_units, results in experiment_results.iteritems():
+        #print 'nb_units ', nb_units
+        train_error, cv_error, test_error = get_errors_from(results)
+        list_units.append(nb_units)
+        list_test_errors.append(test_error)
+    # sort based on first list
+    list_units, list_test_errors = zip(*sorted(zip(list_units, list_test_errors)))
+    return list_units, list_test_errors
+
+##
 
 def get_errors_from(results):
     # get error lists
@@ -13,56 +31,74 @@ def get_results(dirpath, filename):
     train_error, cv_error, test_error = (None, None, None)
     results = None
     path_to_json_file = dirpath+'/'+filename
+    #print 'path_to_json_file', path_to_json_file
     with open(path_to_json_file, 'r') as data_file:
         results = json.load(data_file)
     return results
 
 def get_best_results_from_experiment(experiment_dirpath, list_runs_filenames):
     '''
-        Returns the result structure for the current experiment from all the runs.
+        Returns the best result structure for the current experiment from all the runs.
 
         list_runs_filenames = filenames list with potential runs
     '''
     best_cv_errors = float('inf')
-    best_test_filname = None
-    results_best_mdl = None
+    best_cv_filname = None
+    results_best = None
     for potential_run_filename in list_runs_filenames:
         # if current run=filenmae is a json struct then it has the results
         if 'json' in potential_run_filename:
             run_filename = potential_run_filename
-            results_current_mdl = get_results(experiment_dirpath, run_filename)
-            train_error, cv_error, test_error = get_errors_from(results)
+            results_current_run = get_results(experiment_dirpath, run_filename)
+            train_error, cv_error, test_error = get_errors_from(results_current_run)
             if cv_error < best_cv_errors:
                 best_cv_errors = cv_error
-                best_test_filname = run_filename
-                results_best_mdl = results_current_mdl
-    return results_best_mdl_result, best_test_filname
+                best_cv_filname = run_filename
+                results_best = results_current_run
+    return results_best, best_cv_filname
 
 def get_results_for_experiments(path_to_experiments):
+    '''
+        Returns a dictionary containing the best results for each experiment
+    '''
     experiment_results = {} # maps units -> results_best_mdl e.g {'4':{'results_best_mdl':results_best_mdl}}
     for (experiment_dir, _, potential_runs) in os.walk(path_to_experiments):
         # if current dirpath is a valid experiment and not . (itself)
         if (experiment_dir != path_to_experiments):
-            best_results, best_results_filename = get_best_results_from_experiment(experiment_dirpath=experiment_dir,list_runs_filename=potential_runs)
-            nb_units = results_best_mdl['dims'][1]
-            experiment_results[nb_units] = {'best_results':best_results,'best_results_filename':best_results_filename}
+            results_best, best_filename = get_best_results_from_experiment(experiment_dirpath=experiment_dir,list_runs_filenames=potential_runs)
+            nb_units = results_best['dims'][1]
+            (left, right) = experiment_dir.split('om_results_July_16_jHBF1_')
+            print '--'
+            #print experiment_dir.split('om_results_July_16_jHBF1_')
+            print right[0]
+            print 'experiment_dir ', experiment_dir
+            print 'potential_runs ', len(potential_runs)
+            print 'type(potential_runs)', type(potential_runs)
+            #print 'potential_runs[0]', potential_runs[0]
+            print 'nb_units ', nb_units
+            print 'best_filename ', best_filename
+            experiment_results[nb_units] = results_best
     return experiment_results
+
 ##
 experiment = '/HBF1_multiple'
-path_to_experiments = './om_results_test_experiments/'+experiment
+path_to_experiments = './om_results_test_experiments'+experiment
 multiple_experiment_results = get_results_for_experiments(path_to_experiments)
 
+#pdb.set_trace()
+
 experiment = '/HBF1_single'
-path_to_experiments = './om_results_test_experiments/'+experiment
+path_to_experiments = './om_results_test_experiments'+experiment
 single_experiment_results = get_results_for_experiments(path_to_experiments)
-
-path_to_experiments = path_root + folder_experiments
-print 'path_to_experiments ', path_to_experiments
-# for current experiment, find which is the best run model (best cv)
-
-#for each experiment
-
-single = {} # maps units -> results_best_mdl e.g {'4':{'results_best_mdl':results_best_mdl}}
-
-print 'multiple ', multiple
-#print 'best_test_filname ', best_test_filname
+#
+list_units_multiple, list_test_errors_multiple = get_list_errors(experiment_results=multiple_experiment_results)
+#print list_units_multiple, list_test_errors_multiple
+list_units_single, list_test_errors_single = get_list_errors(experiment_results=single_experiment_results)
+#print list_units_multiple, list_test_errors_single
+#
+plt.figure(3)
+krls.plot_errors(list_units_multiple, list_test_errors_multiple,label='Errors', markersize=3, colour='b')
+krls.plot_errors(list_units_single, list_test_errors_single,label='Errors', markersize=3, colour='b')
+#
+# plt.legend()
+# plt.show()
