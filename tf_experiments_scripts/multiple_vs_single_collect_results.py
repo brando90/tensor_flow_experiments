a26274ed359a0d742b9e9cadd05cfb6be527d14c
Brando <brando90@mit.edu>
2016-07-16 22:06:09 -0400

    reorganize

--- tf_experiments_scripts/multiple_vs_single_collect_results.py
+++ tf_experiments_scripts/multiple_vs_single_collect_results.py
@@ -1,65 +1,68 @@
 import json
 import os
+import pdb
 
-def process_file(dirpath,filename):
-    # train_error, cv_error, test_error = (None, None, None)
-    # results = None
-    if 'json' in filename:
-        path_to_json_file = dirpath+'/'+filename
-        with open(path_to_json_file, 'r') as data_file:
-            results = json.load(data_file) #'train_errors':[], 'cv_errors':[],'test_errors':[]
-            (train_errors, cv_errors, test_errors) = (results['train_errors'], results['cv_errors'], results['test_errors'])
-            #
-            print train_error
-            (train_error, cv_error, test_error) = (train_errors[-1], cv_errors[-1], test_errors[-1])
-            return train_error, cv_error, test_error, results
-    raise('Unexepected error caused by %s'%filename)
+def get_errors_from(results):
+    # get error lists
+    (train_errors, cv_errors, test_errors) = (results['train_errors'], results['cv_errors'], results['test_errors'])
+    # get most recent error
+    (train_error, cv_error, test_error) = (train_errors[-1], cv_errors[-1], test_errors[-1])
+    return train_error, cv_error, test_error
 
+def get_results(dirpath, filename):
+    train_error, cv_error, test_error = (None, None, None)
+    results = None
+    path_to_json_file = dirpath+'/'+filename
+    with open(path_to_json_file, 'r') as data_file:
+        results = json.load(data_file)
+    return results
+
+def get_best_results_from_experiment(experiment_dirpath, list_runs_filenames):
+    '''
+        Returns the result structure for the current experiment from all the runs.
+
+        list_runs_filenames = filenames list with potential runs
+    '''
+    best_cv_errors = float('inf')
+    best_test_filname = None
+    results_best_mdl = None
+    for potential_run_filename in list_runs_filenames:
+        # if current run=filenmae is a json struct then it has the results
+        if 'json' in potential_run_filename:
+            run_filename = potential_run_filename
+            results_current_mdl = get_results(experiment_dirpath, run_filename)
+            train_error, cv_error, test_error = get_errors_from(results)
+            if cv_error < best_cv_errors:
+                best_cv_errors = cv_error
+                best_test_filname = run_filename
+                results_best_mdl = results_current_mdl
+    return results_best_mdl_result, best_test_filname
+
+def get_results_for_experiments(path_to_experiments):
+    experiment_results = {} # maps units -> results_best_mdl e.g {'4':{'results_best_mdl':results_best_mdl}}
+    for (experiment_dir, _, potential_runs) in os.walk(path_to_experiments):
+        # if current dirpath is a valid experiment and not . (itself)
+        if (experiment_dir != path_to_experiments):
+            best_results, best_results_filename = get_best_results_from_experiment(experiment_dirpath=experiment_dir,list_runs_filename=potential_runs)
+            nb_units = results_best_mdl['dims'][1]
+            experiment_results[nb_units] = {'best_results':best_results,'best_results_filename':best_results_filename}
+    return experiment_results
 ##
+experiment = '/HBF1_multiple'
+path_to_experiments = './om_results_test_experiments/'+experiment
+multiple_experiment_results = get_results_for_experiments(path_to_experiments)
 
-# get path to exeriments
-prefix = 'om_results'
-path_root = './%s_test_experiments'%(prefix)
-folder_experiments = '/HBF1_multiple_vs_single'
+experiment = '/HBF1_single'
+path_to_experiments = './om_results_test_experiments/'+experiment
+single_experiment_results = get_results_for_experiments(path_to_experiments)
 
 path_to_experiments = path_root + folder_experiments
 print 'path_to_experiments ', path_to_experiments
 # for current experiment, find which is the best run model (best cv)
-multiple = {} # maps units -> results_best_mdl e.g {'4':{'results_best_mdl':results_best_mdl}}
-single = {} # maps units -> results_best_mdl e.g {'4':{'results_best_mdl':results_best_mdl}}
-#for each experiment
-for (dirpath, dirnames, filenames) in os.walk(path_to_experiments):
-    # dirpath = current directory we are exploring (experiment dir)
-    # dirnames = directories inside of dirpath
-    # filenames = filenames inside dirpath (json files with experiments)
 
-    best_cv_errors = float("inf")
-    best_test_filname = None
-    results_best_mdl = None
-    # if current dirpath is a valid experiment
-    if 'multiple' in dirpath or 'single' in dirpath:
-        # go through all experiment runs in dirpath
-        for filename  in filenames:
-            (train_error, cv_error, test_error, results_current_mdl) = process_file(dirpath,filename)
-            if cv_error < best_cv_errors:
-                print 'relax'
-                best_cv_errors = cv_error
-                best_test_filname = filename
-                results_best_mdl = results_current_mdl
-        # now we have the best model (and hyperparams) for this experiment
-        nb_units = results_best_mdl['dims'][1]
-        #collect results in proper directory
-        if 'multiple' in dirpath:
-            experiment_dir = multiple
-        elif 'single' in dirpath:
-            experiment_dir = single
-        else:
-            raise('Invalid experiment: ', dirpath)
-        experiment_dir[nb_units] = {} # dict to collect data for best experiment
-        experiment_dir[nb_units]['results_best_mdl'] = results_best_mdl
-        experiment_dir[nb_units]['best_test_filname'] = best_test_filname
-    else:
-        pass
+#for each experiment
+
+single = {} # maps units -> results_best_mdl e.g {'4':{'results_best_mdl':results_best_mdl}}
 
 print 'multiple ', multiple
 #print 'best_test_filname ', best_test_filname
