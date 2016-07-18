fc7ddaab06aa523d8d1cc999770bc30e9b41a14c
Brando <brando90@mit.edu>
2016-07-18 18:53:25 -0400

    stuff

--- tf_experiments_scripts/sbatch_om_script.sh
+++ tf_experiments_scripts/sbatch_om_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=HBF1_4_multiple_S_random_HP
+#SBATCH --job-name=HBF1_2_multiple_S_random_HP
 #SBATCH --nodes=1
 #SBATCH --mem=7000
 #SBATCH --time=6-23
-#SBATCH --array=1-2
+#SBATCH --array=1-10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID HBF1_2_multiple_S_random_HP_test True om_results multiple_S_test
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID HBF1_2_multiple_S_random_HP_test True om_results multiple_S_test 2
 
