21e7502a799ba0aac30c3162f38ba8956a5c048a
Brando <brando90@mit.edu>
2016-07-21 16:13:22 -0400

    sbatch krls

--- tf_experiments_scripts/sbatch_krls.sh
+++ tf_experiments_scripts/sbatch_krls.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=HBF1_6_multiple_S_random_HP
+#SBATCH --job-name=KRLS
 #SBATCH --nodes=1
 #SBATCH --mem=7000
 #SBATCH --time=6-23
-#SBATCH --array=1-25
+#SBATCH --array=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID HBF1_6_multiple_S_random_HP_test True om_results multiple_S_test 6
+python krls_collect_data.py f_2D_task2 ./tmp_result_krls_f_2D_task2_results 50 50 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34
+
+python krls_collect_data.py f_2D_task2 ./tmp_result_krls_f_2D_task2_results 3 3 2,3,4
