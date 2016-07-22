45693bd70bee03aac379156c76240ebcf17813ce
Brando <brando90@mit.edu>
2016-07-22 02:32:57 -0400

    check if it works

--- tf_experiments_scripts/sbatch_om_script.sh
+++ tf_experiments_scripts/sbatch_om_script.sh
@@ -7,5 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rene_sax14@yahoo.com
 
-python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID HBF1_6_multiple_S_random_HP_test True om_results multiple_S_test 6
-
+python main_nn.py $SLURM_JOBID $SLURM_ARRAY_TASK_ID HBF1_12_multiple_S_random_HP_task2_test True om_results multiple_S 2 multiple_S f_2D_task2
