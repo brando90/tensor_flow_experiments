87a37c48b093a3d680bec90db84f305b79d606be
Brando <brando90@mit.edu>
2016-07-24 01:46:18 -0400

    MNIST

--- tf_experiments_scripts/krls_plot_errrors_script.py
+++ tf_experiments_scripts/krls_plot_errrors_script.py
@@ -64,17 +64,20 @@ if __name__ == '__main__':
     task_name = 'f_2d_task2_xsinglog1_x_depth2'
     # task_name = 'f_2d_task2_xsinglog1_x_depth3'
 
-    experiments_root = './tmp_krls_experiments'
+    # experiments_root = './tmp_krls_experiments'
+    # experiment_dir = '/July_24_krls_f_2d_task2_xsinglog1_x_depth2'
+    # results_filename = '/results_json_July_24_krls_f_2d_task2_xsinglog1_x_depth2'
+    # rbf_params_filename = '/rbf_params_July_24_krls_f_2d_task2_xsinglog1_x_depth2.npz'
 
-    experiment_dir = '/July_23_krls_f_2d_task2_xsinglog1_x_depth2'
-    results_filename = '/results_json_July_23_krls_f_2d_task2_xsinglog1_x_depth2'
-    rbf_params_filename = '/rbf_params_July_23_krls_f_2d_task2_xsinglog1_x_depth2.npz'
+    # experiments_root = './om_krls_experiments'
+    # experiment_dir = '/July_22_krls_experiment_30_30_D_6_12_24_48_96'
+    # results_filename = '/results_json_July_22_krls_experiment_30_30_D_6_12_24_48_96'
+    # rbf_params_filename = '/rbf_params_July_22_krls_experiment_30_30_D_6_12_24_48_96.npz'
 
-    #experiments_root = './om_krls_experiments'
-
-    #experiment_dir = '/July_22_krls_experiment_30_30_D_6_12_24_48_96'
-    #results_filename = '/results_json_July_22_krls_experiment_30_30_D_6_12_24_48_96'
-    #rbf_params_filename = '/rbf_params_July_22_krls_experiment_30_30_D_6_12_24_48_96.npz'
+    experiments_root = './om_krls_experiments'
+    experiment_dir = '/July_24_krls_task2_xsinglog1_x_depth_3_10_10_D_6_12_24_48_96_182_246_364'
+    results_filename = '/results_json_July_24_krls_task2_xsinglog1_x_depth_3_10_10_D_6_12_24_48_96_182_246_364'
+    rbf_params_filename = '/rbf_params_July_24_krls_task2_xsinglog1_x_depth_3_10_10_D_6_12_24_48_96_182_246_364.npz'
 
     #experiment_dir = '/July_22_krls_experiment_10_10_D_6_12_24_48_96_units_search'
     #results_filename = '/results_json_July_22_krls_experiment_10_10_D_6_12_24_48_96_units_search'
