68da53c9c13a760f4b97552de65b2161ca51a380
Brando <brando90@mit.edu>
2016-07-25 13:47:09 -0400

    hrushiketsh

---
+++ tf_experiments_scripts/get_sense_norm.py
@@ -0,0 +1,16 @@
+from sklearn.metrics.pairwise import euclidean_distances
+import numpy as np
+
+import my_tf_pkg as mtf
+
+def get_pairwise_norm_squared(X,Y):
+    return mtf.euclidean_distances(X=X,Y=Y,squared=True)
+
+task_name = 'hrushikesh'
+X_train, Y_train, X_cv, Y_cv, X_test, Y_test = mtf.get_data(task_name)
+
+pairwise_norm_squared = get_pairwise_norm_squared(X=X_train,Y=X_train)
+print 'min', np.amin(pairwise_norm_squared)
+print 'max', np.amax(pairwise_norm_squared)
+print 'mean', np.mean(pairwise_norm_squared)
+print 'std', np.std(pairwise_norm_squared)
---
+++ tf_experiments_scripts/sbatch_krls_hrushikesh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=KRLS
+#SBATCH --nodes=1
+#SBATCH --mem=7000
+#SBATCH --time=6-23
+#SBATCH --array=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rene_sax14@yahoo.com
+
+python krls_collect_data.py hrushikesh om_krls krls_hrushikesh_flat_50_50_units_6_12_24_48_96_182_246_363 2 2 6,12,24,48,96,182,246,363
