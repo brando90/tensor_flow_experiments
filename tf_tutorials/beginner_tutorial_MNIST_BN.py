6881450d59a2f252efe172accded3005bd0cb38f
Brando <brando90@mit.edu>
2016-07-28 00:16:08 -0400

    BN MNIST example

--- tf_tutorials/beginner_tutorial_MNIST_BN.py
+++ tf_tutorials/beginner_tutorial_MNIST_BN.py
@@ -51,58 +51,75 @@ def softmax_layer(l, x, dims, init):
 ###
 ###
 
+def get_batch_feed(M, phase_train, x,y_):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    feed_dict = {x: batch_xs, y_: batch_ys, phase_train: True} if (phase_train is not None) else {x: batch_xs, y_: batch_ys}
+    return feed_dict
+
 def build_NN_two_hidden_layers(x, phase_train):
     ## first layer
-    shape_layer1 = [784,50]
-    init_W,init_b = tf.contrib.layers.xavier_initializer(dtype=tf.float64), tf.constant(0.1, shape=shape_layer1[1])
-    A1 = get_NN_layer(l=1, x=x, dims=shape_layer1, init=(init_W,init_b), phase_train=phase_train, scope="NNLayer")
+    [D_in,D_out] = [784,50]
+    init_W,init_b = tf.contrib.layers.xavier_initializer(dtype=tf.float64), tf.constant(0.1, shape=[D_out])
+    A1 = get_NN_layer(l='1', x=x, dims=[D_in,D_out], init=(init_W,init_b), phase_train=phase_train, scope="NNLayer")
     ## second layer
-    shape_layer2 = [50,49]
-    init_W,init_b = tf.contrib.layers.xavier_initializer(dtype=tf.float64), tf.constant(0.1, shape=shape_layer2[1])
-    A2 = get_NN_layer(l=2, x=x, dims=shape_layer1, init=(init_W,init_b), phase_train=phase_train, scope="NNLayer")
+    [D_in,D_out] = [50,49]
+    init_W,init_b = tf.contrib.layers.xavier_initializer(dtype=tf.float64), tf.constant(0.1, shape=[D_out])
+    A2 = get_NN_layer(l='2', x=x, dims=[D_in,D_out], init=(init_W,init_b), phase_train=phase_train, scope="NNLayer")
     ## final layer
     y = softmax_layer(A2)
     return y
 
-x = tf.placeholder(tf.float32, [None, 784])
-y = build_NN_two_hidden_layers(x, phase_train)
+def get_feed_for_learning(phase_train, X_train,X_cv,X_test, Y_train,Y_cv,Y_test,  x,y_):
+    '''
+        Returns the feed_dict for sess.run when BN is present or not.
+    '''
+    if phase_train is not None: #DO BN
+        feed_dict_train, feed_dict_cv, feed_dict_test = {x:X_train, y_:Y_train, phase_train: False}, {x:X_cv, y_:Y_cv, phase_train: False}, {x:X_test, y_:Y_test, phase_train: False}
+    else: #Don't do BN
+        feed_dict_train, feed_dict_cv, feed_dict_test = {x:X_train, y_:Y_train}, {x:X_cv, y_:Y_cv}, {x:X_test, y_:Y_test}
+    return feed_dict_train, feed_dict_cv, feed_dict_test
 
-cross_entropy = tf.reduce_mean( -tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]) )
-# single training step opt.
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-#init op
-init = tf.initialize_all_variables()
-# launch model in a session
-sess = tf.Session()
-#run opt to initialize
-sess.run(init)
+def get_MNIST_data_sets():
+    X_train, Y_train = mnist.train.images, mnist.train.labels
+    X_cv, Y_cv = mnist.validation.images, mnist.validation.labels
+    X_test, Y_test = mnist.test.images, mnist.test.labels
+    return X_train,X_cv,X_test, Y_train,Y_cv,Y_test
 
-##BN ON or OFF
-bn = False
-phase_train = tf.placeholder(tf.bool, name='phase_train') if bn else  phase_train ##BN ON
+def main():
+    ##BN ON or OFF
+    bn = False
+    phase_train = tf.placeholder(tf.bool, name='phase_train') if bn else  None
+    ##
+    x = tf.placeholder(tf.float64, [None, 784])
+    y = build_NN_two_hidden_layers(x, phase_train)
+    ### training
+    # new placeholder to input the correct answers
+    y_ = tf.placeholder(tf.float64, [None, 10])
+    # loss function
+    cross_entropy = tf.reduce_mean( -tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]) )
+    # single training step opt.
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    #init op
+    init = tf.initialize_all_variables()
+    # launch model in a session
+    sess = tf.Session()
+    #run opt to initialize
+    sess.run(init)
 
-if phase_train is not None:
-    #DO BN
-    feed_dict_train, feed_dict_cv, feed_dict_test = {x:X_train, y_:Y_train, phase_train: False}, {x:X_cv, y_:Y_cv, phase_train: False}, {x:X_test, y_:Y_test, phase_train: False}
-else:
-    #Don't do BN
-    feed_dict_train, feed_dict_cv, feed_dict_test = {x:X_train, y_:Y_train}, {x:X_cv, y_:Y_cv}, {x:X_test, y_:Y_test}
+    feed_dict_train, feed_dict_cv, feed_dict_test = get_feed_for_learning(phase_train, X_train,X_cv,X_test, Y_train,Y_cv,Y_test, x,y_)
 
-def get_batch_feed(M, phase_train):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    feed_dict = {x: Xminibatch, y_: Yminibatch, phase_train: True} if (phase_train is not None) else {x: Xminibatch, y_: Yminibatch}
-    return feed_dict
+    # we'll run the training step 1000 times
+    for i in range(1000):
+        #batch_xs, batch_ys = mnist.train.next_batch(100)
+        batch_xs, batch_ys = get_batch_feed(M, phase_train)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-# we'll run the training step 1000 times
-for i in range(1000):
-#batch_xs, batch_ys = mnist.train.next_batch(100)
-    batch_xs, batch_ys = get_batch_feed(M, phase_train)
-    get_batch_feed(X_train, Y_train, M, phase_train)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    # list of booleans indicating correct predictions
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    # accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
 
-# list of booleans indicating correct predictions
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-# accuracy
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+if __name__ == '__main__':
+    main()
